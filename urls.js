015e309cfb3a1fe4a110de004f627429505ae309
Ayoub22232223 <ayoubpro2223@gmail.com>
2025-04-30 03:08:06 -1000

    urls.js

---
+++ urls.js
@@ -0,0 +1,189 @@
+var ConfiguracionCanales = {
+    "sharq": {
+        url: "https://svs.itworkscdn.net/asharqdocumentarylive/asharqdocumentary.smil/playlist.m3u8"
+    },
+  
+  "beinn": {
+        url: "https:\/\/cdn-bein-prod-a-01.vos360.video\/Content\/Channel\/svc-spo-hd-39-dt\/DASH\/playlist.mpd",
+        k1: "7faafe485db7395b9583222a3a6e789d",
+        k2: "163906ba32dd3d611aad5789cbd00858"
+    },
+  "beins": {
+        url: "https:\/\/cdn-bein-prod-a-01.vos360.video\/Content\/Channel\/svc-spo-hd-38-dt\/DASH\/playlist.mpd",
+        k1: "dece176768f639bcbaf97ebca9c74164",
+        k2: "dd5f1f0bfe87b23eb94160af51671670"
+    },
+    "bein1": {
+        url: "https://cdn-bein-prod-a-01.vos360.video/Content/Channel/svc-spo-hd-08-dt/DASH/playlist.mpd",
+        k1: "39471a4508f1387d96867b6e8ffa1e39",
+        k2: "ddd1fa7cd620953469679dca5bc19e54"
+    },
+    "natgeo": {
+        url: "https://uselector.cdn.intigral-ott.net/NGAD/NGAD.isml/manifest.mpd",
+        k1: "b1e2ae3754ced9e9258a8df46d261390",
+        k2: "bc42486b5253920abdd0cb3e727852f5"
+    },
+    "jadoc": {
+        url: "https://live-hls-web-ajd.getaj.net/AJD/index.m3u8"
+    },
+    "ad1": {
+        url: "https://vo-live-media.cdb.cdn.orange.com/Content/Channel/AbuDhabiSportsChannel1/HLS/index.m3u8"
+    },
+    "ad2": {
+        url: "https://vo-live.cdb.cdn.orange.com/Content/Channel/AbuDhabiSportsChannel2/HLS/index.m3u8"
+    },
+    "adasia1": {
+        url: "https://adasia1.starzplayarabia.com/out/v1/3c0cb95505b14665996b96a9abf5f18c/index.mpd",
+        k1: "f88a4bd31ed8999cc56a843690635970",
+        k2: "d481dd51e34364884356b61ee9a28d1c"
+    },
+    "adpr2s2": {
+        url: "https://fastly-live.cdn.intigral-ott.net/ADSP2/ADSP2.isml/manifest.mpd",
+        k1: "efca93272b9d34f5cf9d8598f43be5ea",
+        k2: "55e5fe489e9c18f6c979c3126d4bcfed"
+    },
+    "AD1": {
+        url: "https://uselector.cdn.intigral-ott.net/ADSP1/ADSP1.isml/manifest.mpd",
+        k1: "eb5a3a48f3e191a00e3ac1e2d470c491",
+        k2: "2c8b7198563527e524d66628c092ef1f"
+    },
+    "AD2": {
+        url: "https://uselector.cdn.intigral-ott.net/ADSP2/ADSP2.isml/manifest.mpd",
+        k1: "efca93272b9d34f5cf9d8598f43be5ea",
+        k2: "55e5fe489e9c18f6c979c3126d4bcfed"
+    },
+    "dubai1": {
+        url: "https://dmidspta.cdn.mgmlcdn.com/dubaisports/smil:dubaisports.stream.smil/chunklist.m3u8"
+    },
+    "dubai2": {
+        url: "https://dmitwlvvll.cdn.mgmlcdn.com/dubaisportshd/smil:dubaisportshd.smil/chunklist.m3u8"
+    },
+    "dubai3": {
+        url: "https://dmitwlvvll.cdn.mgmlcdn.com/dubaisportshd5/smil:dubaisportshd5.smil/chunklist.m3u8"
+    },
+    "mbc+": {
+        url: "https://mbcplusdrama-prod-dub-ak.akamaized.net/out/v1/97ca0ce6fc6142f4b14c0a694af59eab/index.m3u8"
+    },
+    "alkas1": {
+        url: "https://3rbcafee.glitch.me/https://liveakgr.alkassdigital.net/hls/live/2097037/Alkass1mhua/master.m3u8"
+    },
+    "alkas2": {
+        url: "https://3rbcafee.glitch.me/https://liveakgr.alkassdigital.net/hls/live/2097037/Alkass2hef/master.m3u8"
+    },
+    "alkas3": {
+        url: "https://3rbcafee.glitch.me/https://liveakgr.alkassdigital.net/hls/live/2097037/Alkass3vak/master.m3u8"
+    },
+    "alkas4": {
+        url: "https://zealous-efficient-shoemaker.glitch.me/https://liveakgr.alkassdigital.net/hls/live/2097037/Alkass4cna/master.m3u8"
+    },
+    "alkas5": {
+        url: "https://3rbcafee.glitch.me/https://liveakgr.alkassdigital.net/hls/live/2097037/Alkass5nnha/master.m3u8"
+    },
+    "alkas6": {
+        url: "https://3rbcafee.glitch.me/https://liveakgr.alkassdigital.net/hls/live/2097037/Alkass6buzay/master.m3u8"
+    },
+    "shoof": {
+        url: "https://3rbcafee.glitch.me/https://liveakgr.alkassdigital.net/hls/live/2097037/Alkass6Shoof1/master.m3u8"
+    },
+    "mbcmsr": {
+        url: "https://uselector.cdn.intigral-ott.net/MBMS2/MBMS2.isml/manifest.mpd",
+        k1: "4da4fec209622c5da04762e8d2d2f1d7",
+        k2: "4495168cf1a99038546b9d042dbe7c97"
+    },
+    "ssc4k": {
+        url: "https://4k-event2-new-25fps-ak.akamaized.net/out/v1/c40564684d074b29a21efcf70725df3c/index.mpd",
+        k1: "73efb1eb27294f8ea059765b368f411b",
+        k2: "8af553e93bc5b20e828a14a7643e0c59"
+    },
+    "SSC1": {
+        url: "https://ssc-1-enc.edgenextcdn.net/out/v1/c696e4819b55414388a1a487e8a45ca1/index.mpd",
+        k1: "d84c325f36814f39bbe59080272b10c3",
+        k2: "550727de4c96ef1ecff874905493580f"
+    },
+    "SSC2": {
+        url: "https://ssc-2-enc.edgenextcdn.net/out/v1/a16db2ec338a445a82d9c541cc9293f9/index.mpd",
+        k1: "8BCFC55359E24BD7AD1C5560A96DDD3C",
+        k2: "b5dcf721ab522af92a9d3bf0bd55b596"
+    },
+    "SSC3": {
+        url: "https://ssc-3-enc.edgenextcdn.net/out/v1/42e86125555242aaa2a12056832e7814/index.mpd",
+        k1: "7de5dd08ad8041d586c2f16ccc9490a1",
+        k2: "5e1503f3398b34f5099933fedab847ef"
+    },
+    "SSC4": {
+        url: "https://ssc-4-enc.edgenextcdn.net/out/v1/5267ea5772874b0db24559d643eaad93/index.mpd",
+        k1: "5c672f6b85a94638872d0214f7806ed4",
+        k2: "bf8756fbb866ee2d5c701c2289dd8de3"
+    },
+    "SSC5": {
+        url: "https://ssc-5-enc.edgenextcdn.net/out/v1/99289eac5a7b4319905da595afbd792b/index.mpd",
+        k1: "c88b512b17ab4f6cb09eb0ff4a1056ed",
+        k2: "adc08ee1c20a734972a55c9aebbd1888"
+    },
+    "sscext1": {
+        url: "https://ssc-extra-1-enc.edgenextcdn.net/out/v1/647c58693f1d46af92bd7e69f17912cb/index.mpd",
+        k1: "ecbc9e6fe6b145efb6658fb5cf7427f8",
+        k2: "03c17e28911f71221acbc0b11f900401"
+    },
+    "sscext2": {
+        url: "https://ssc-extra-2-enc.edgenextcdn.net/out/v1/8b70de2b70d447ba8a7450ba90926a2d/index.mpd",
+        k1: "4d89249bd4ca4ebc9e70443265f9507d",
+        k2: "cf074ffd2646c9c2f8513b47fa57bc30"
+    },
+    "sscext3": {
+        url: "https://ssc-extra3-enc.edgenextcdn.net/out/v1/8f1c6c3f05ef4284a64b342891bd85ae/index.mpd",
+        k1: "98cfd6fd4812497fb24dc75f7545f2ee",
+        k2: "d3006ee69e77b25939728ebf30d3180a"
+    },
+    "mbc1": {
+        url: "https://mbc1na-prod-dub-enc.edgenextcdn.net/out/v1/84ab37e99d6e4b16b33c6600f94f6ccb/index.m3u8"
+    },
+    "mbc3": {
+        url: "https://fastly-live.cdn.intigral-ott.net/MB3/MB3.isml/manifest.mpd",
+        k1: "ed5ca9f7aafa02e7ba138eff7c021c8f",
+        k2: "f27dae60067ad34b5a921af79a041372"
+    },
+    "Cartoon": {
+        url: "https://fastly-live.cdn.intigral-ott.net/CNA/CNA.isml/manifest.mpd",
+        k1: "ece7bbc683eb35d1e6b3e32cccbd8fb0",
+        k2: "f72bd6ea26e243524d58993b58122313"
+    },
+    "mbc4": {
+        url: "https://mbc4-prod-dub-enc.edgenextcdn.net/out/v1/c08681f81775496ab4afa2bac7ae7638/index.m3u8"
+    },
+    "mbcact": {
+        url: "https://fastly-live.cdn.intigral-ott.net/MBAH/MBAH.isml/manifest.mpd",
+        k1: "5567531458e9c2e93d22935c45fdef52",
+        k2: "f2f613fb2132acd019d2a96bf1be15c1"
+    },
+    "mbcmax": {
+        url: "https://fastly-live.cdn.intigral-ott.net/MBMH/MBMH.isml/manifest.mpd",
+        k1: "0a414b719bade2acc124e22c042290f3",
+        k2: "db84a5afa10a001dd73b094c14638212"
+    },
+    "mbcdrama": {
+        url: "https://mbc1-enc.edgenextcdn.net/out/v1/b0b3a0e6750d4408bb86d703d5feffd1/index_21.m3u8"
+    },
+    "mbcmasr": {
+        url: "https://mbc1-enc.edgenextcdn.net/out/v1/d5036cabf11e45bf9d0db410ca135c18/index.m3u8"
+    },
+    "mbcdm": {
+        url: "https://shls-live-enc.edgenextcdn.net/out/v1/08eca926a78a41339b8010c882410307/index.m3u8"
+    },
+    "mbcd1": {
+        url: "https://mbcplusdrama-prod-dub-enc.edgenextcdn.net/out/v1/97ca0ce6fc6142f4b14c0a694af59eab/index.m3u8"
+    },
+    "mbc2": {
+        url: "https://fastly-live.cdn.intigral-ott.net/MB2H/MB2H.isml/manifest.mpd",
+        k1: "baa1907a284502eabc320d77051c575c",
+        k2: "58fa20cf6055f3aefc15707992c0b685"
+    },
+    "mbc5": {
+        url: "https://mbc5-prod-dub-enc.edgenextcdn.net/out/v1/2720564b6a4641658fdfb6884b160da2/index.m3u8"
+    },
+    "sscnews": {
+        url: "https://ssc-news-live-enc.edgenextcdn.net/out/v1/ef466f43623c4bbaa3f905b566ec35ea/index.mpd",
+        k1: "3d04975236a44f62857d181597705ee6",
+        k2: "362133e9cb13189ad4fe095ced216f60"
+    }
+};
